9fc666378f5edca4f0e9d387caa53e8ab945718f
sathish19961412 <sathishfreelanc5.com>
2024-10-27 10:33:34 -0400

    create parent and child compents data transfer to P to C

--- src/CustomerAdd.js
+++ src/CustomerAdd.js
@@ -1,21 +1,24 @@
 import React,{useState} from "react";
+import CustomerView from "./CustomerView";
 
 export default function CustomerAdd(){
-
+ 
     const [input,setInput]=useState("");
-    const [customer,setCustomer]=useState([]);
+    const [customers,setCustomers]=useState([]);
 
     function addCustomer(){
-        if(input){
-            setCustomer((previousState)=>[...previousState,input])
-            console.log(customer);
+        if(input)
+        {
+            setCustomers((previousState)=>[...previousState,input])
+            setInput("");
         }
     }
     return(
         <>
             <h3>Add New Customer</h3>
-            <input type="text" onChange={(e)=>setInput(e.target.value)} />
+            <input type="text" value={input} onChange={(e)=>(setInput(e.target.value))}/>
             <button onClick={addCustomer}>Add</button>
+            <CustomerView customers={customers}/>
         </>
     )
 }
